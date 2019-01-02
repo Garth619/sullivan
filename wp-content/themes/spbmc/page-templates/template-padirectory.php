c3f68dd7ae93d8267010bd3126c7cfd05d174ed2
Garrett Cullen <garrettwcullen@gmail.com>
2019-01-02 10:20:40 -0800

    push

--- wp-content/themes/spbmc/page-templates/template-padirectory.php
+++ wp-content/themes/spbmc/page-templates/template-padirectory.php
@@ -5,8 +5,43 @@
 get_header(); ?>
 
 
+<div id="internal_main" class="">
+
+
+<div class="internal_banner">
+	
+	<img src="<?php bloginfo('template_directory');?>/images/hero-intl.jpg"/>
+	
+	<div class="internal_banner_content">
+		
+		<span class="large_header">Cases We Handle</span><!-- large_header -->
+		
+		<a class="button" href="#consultation">Click for free consultation</a>
+		
+	</div><!-- internal_banner_content -->
+	
+</div><!-- internal_banner -->
+
+
+<div class="container one_col">
+	
+	<div class="pa_directory_wrapper">
 		
 		<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'pa_menu' ) ); ?>
+		
+	</div><!-- pa_directory_wrapper -->
+		
+	
+</div><!-- container -->
+
+
+
+</div><!-- internal_main -->
+
+
+
+		
+		
 
 
 <?php get_footer(); ?>
--- wp-content/themes/spbmc/page.php
+++ wp-content/themes/spbmc/page.php
@@ -3,7 +3,19 @@
 <div id="internal_main" class="">
 
 
-<?php get_template_part( 'page-templates/internal', 'banner' ); ?>
+<div class="internal_banner">
+	
+	<img src="<?php bloginfo('template_directory');?>/images/hero-intl.jpg"/>
+	
+	<div class="internal_banner_content">
+		
+		<span class="large_header">New York Personal Injury Attorneys </span><!-- large_header -->
+		
+		<a class="button" href="#consultation">Click for free consultation</a>
+		
+	</div><!-- internal_banner_content -->
+	
+</div><!-- internal_banner -->
 
 
 <div class="container two_col">
@@ -13,42 +25,10 @@
 	
 	<div class="content_wrapper content">
 		
-		<h1>H1 - New York Firefighter Injury Attorney </h1>
+		<h1><?php the_title();?></h1>
 		
-		<p>Aliquam eget ullamcorper magna. Nam id convallis turpis. Mauris porta diam augue. Sed sit 		amet nisl nec arcu mollis faucibus tempor id ante. Curabitur commodo vestibulum nulla at 		lacinia. Cras venenatis, massa et imperdiet ultrices, <a href="">embedded link</a> imperdiet magna, nec mollis ante nisl vitae eros. Curabitur consequat facilis xis lectus a consequat. Aliquam erat volutpat. Sed est lacus, egestas a risus posuere, pharetra elementum risus. Maecenas ullamcorper tortor ac eros adipiscing, ac commodo purus varius. Curabitur placerat nec leo quis porta.</p>
-		
-		<p>Aliquam eget ullamcorper magna. Nam id convallis turpis. Mauris porta diam augue. Sed sit 		amet nisl nec arcu mollis faucibus tempor id ante. Curabitur commodo vestibulum nulla at 		lacinia. Cras venenatis, massa et imperdiet ultrices, embedded link   embedded link rollover imperdiet magna, nec mollis ante nisl vitae eros. Curabitur consequat facilis xis lectus a consequat. Aliquam erat volutpat. Sed est lacus, egestas a risus posuere, pharetra elementum risus. Maecenas ullamcorper tortor ac eros adipiscing, ac commodo purus varius. Curabitur placerat nec leo quis porta.</p>
+		<?php get_template_part( 'loop', 'page' ); ?>
 
-		<h2>H2 - ullam tempus orci lacus sit amet itels scelerisque ligula vestibulum vitae lacus sit amet itels scelerisque ligula</h2>
-
-		<p>Curabitur eu ornare sapien. Praesent non adipiscing nisi. Aenean adipiscing risus eget feugiat pharetra. Proin malesuada sem lectus, et facilisis ante condimentum a. Phasellus nec tellus tempor, adipiscing orci sit amet, dapibus magna. Phasellus ultricies lacus vitae pharetra pharetra. Nulla eleifend, risus non semper lobortis, massa eros molestie nulla, eget adipiscing felis neque ultrices justo. Quisque facilisis magna ut erat laoreet adipiscing.</p> 
-
-		<h3>H3 - ullam tempus orci lacus sit amet itels scelerisque ligula vestibulum vitae lacus sit amet itels scelerisque ligula</h3>
-			
-		<p>Pellentesque non suscipit nisi, vel ultricies tellus. Etiam vel porttitor turpis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tempus orci lacus, sit amet scelerisque ligula vestibulum vitae. Mauris faucibus, urna a pharetra venenatis, velit diam tempor sem, nec ornare felis diam sit amet turpis. Aliquam eget ullamcorper magna. Nam id convallis turpis. Mauris porta diam augue. Sed sit amet nisl nec arcu mollis faucibus tempor id ante. Curabitur commodo vestibulum nulla at lacinia.</p>
-
-<blockquote>“ Pullquote : The attorneys and support staff of this firm are unwavering in their commitment to our clients. This is demonstrated by the results that we have achieved over the years.”</blockquote>
-
-<ul>
-
-<li>Curabitur eu ornare sapien. Praesent non adipiscing nisi. Aenean adipiscing risus eget feugiat pharetra. Proin malesuada sem lectus, et facilisis ante condimentum a.</li>
-<li>Phasellus nec tellus tempor, adipiscing orci sit amet, dapibus magna. Phasellus ultricies lacus vitae pharetra pharetra. Nulla eleifend, risus non semper lobortis, massa eros molestie nulla.</li>
-
-</ul>
-
-<p>Pellentesque non suscipit nisi, vel ultricies tellus. Etiam vel porttitor turpis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tempus orci lacus, sit amet scelerisque ligula vestibulum vitae. Mauris faucibus, urna a pharetra venenatis, velit diam tempor sem, nec ornare felis diam sit amet turpis. Aliquam eget ullamcorper magna. Nam id convallis turpis. Mauris porta diam augue. Sed sit amet nisl nec arcu mollis faucibus tempor id ante. Curabitur commodo vestibulum nulla at lacinia.</p>
-
-<ol>
-<li>Pellentesque non suscipit nisi, vel ultricies tellus. Etiam vel porttitor turpis. Lorem ipsum dolor sit amet,  	       consectetur adipiscing elit. Nullam tempus orci lacus, sit amet scelerisque ligula vestibulum vitae.</li>
-<li> Mauris faucibus, urna a pharetra venenatis, velit diam tempor sem, nec ornare felis diam sit amet turpis. 	       Aliquam eget ullamcorper magna. Nam id convallis turpis. Mauris porta diam augue.</li>
-</ol>
-
-<p>Cras nec elit ac augue pretium commodo non id mi. Proin eleifend consequat purus ac aliquam. Etiam arcu leo, pellenvtesque ac pharetra et, ultrices bibendum massa. Cras arcu metus, dictum in erat id, semper condimentum ligula. Etiam tempor tortor eu urna rutrum, eu viverra dolor rutrum. Curabitur vel convallis purus. In sit amet viverra augue. Duis tincidunt dolor nec massa fermentum porta. Nullam id congue purus. In semper lacus et blandit laoreet.</p>
-
-
-		
-		
-		
 	</div><!-- content_wrapper -->
 	
 	
@@ -57,12 +37,6 @@
 
 
 </div><!-- internal_main -->
-
-
-		
-	<?php // get_template_part( 'loop', 'page' ); ?>
-		
-
 
 
 
