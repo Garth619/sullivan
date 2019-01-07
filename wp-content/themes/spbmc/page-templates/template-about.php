ddfa5492e57afd3abf39b2b8e069a78d06f6bc53
Garrett Cullen <garrettwcullen@gmail.com>
2019-01-07 12:34:14 -0800

    push

--- wp-content/themes/spbmc/page-templates/template-about.php
+++ wp-content/themes/spbmc/page-templates/template-about.php
@@ -4,12 +4,23 @@
 
 get_header(); ?>
 
+<div id="internal_main" class="">
 
 
-		
-		<?php get_template_part( 'loop', 'page' ); ?>
-		
+<div class="container no_banner_col">
 	
+	<div class="internal_header">
+			
+		<h1><?php the_title();?></h1>
+			
+	</div><!-- internal_header -->
+	
+		
+	</div><!-- container -->
 
 
+</div><!-- internal_main -->
+
+					 
+					 	
 <?php get_footer(); ?>
