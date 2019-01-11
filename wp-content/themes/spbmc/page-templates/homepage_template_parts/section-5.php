ed107de1baf543a279059be39ac836f5661b469c
Garrett Cullen <garrettwcullen@gmail.com>
2019-01-11 09:27:04 -0800

    push

---
+++ wp-content/themes/spbmc/page-templates/homepage_template_parts/section-5.php
@@ -0,0 +1,73 @@
+<section id="section_five">
+	
+	
+		
+		<div class="sec_five_header">
+			
+			<span class="top">Award</span>
+			<span class="middle">Winning</span>
+			<span class="bottom">Attorneys</span>
+			
+		</div><!-- sec_five_header -->
+		
+		<div class="sec_five_content top content">
+			
+			<p>Aliquam eget ullamcorper magna. Nam id convallis turpis. Mauris porta diam augue. Sed sit amet nisl nec arcu mollis faucibus tempor id ante. Curabitur commodo vestibulum nulla at lacinia. Cras venenatis, massa et imperdiet ultrices, ante nisl imperdiet magna, nec mollis ante nisl vitae eros. Curabitur consequat facilisis lectus a consequat. Aliquam erat volutpat. Sed est lacus, egestas a risus posuere, pharetra elementum risus. Maecenas ullamcorper tortor ac eros adipiscing, ac commodo purus varius. Curabitur placerat nec leo quis porta.</p>
+
+			<h2>Nullam tempus orci lacus sit amet itels scelerisque ligula vestibulum vitae lacus sit amet itels scelerisque ligula vestibulum</h2>
+
+			<p>Nam pharetra, dui semper imperdiet dictum, ipsum augue auctor nibh, vel porta velit diam eget neque. Nullam fermentum mauris eu massa iaculis, et gravida dolor tincidunt. Duis et pellentesque augue, quis porttitor ligula. Nunc sit amet sem in dolor euismod rutrum. Suspendisse potenti. Quisque at consequat urna. Nam lobortis elementum sapien, a blandit tortor placerat ac. Nam porttitor consectetur dui, sed rhoncus nisl porttitor non. Curabitur eu ornare sapien. Praesent non adipiscing nisi. Aenean adipiscing risus eget feugiat pharetra. Proin malesuada sem lectus, et facilisis ante condimentum a. Phasellus nec tellus tempor, adipiscing orci sit amet, dapibus magna. Phasellus ultricies lacus vitae pharetra pharetra.</p> 
+			
+			<blockquote>“The attorneys and support staff of this firm are unwavering in their commitment to our clients. This is demonstrated by the results that we have achieved over the years.”</blockquote>
+			
+			<p>Aliquam eget ullamcorper magna. Nam id convallis turpis. Mauris porta diam augue. Sed sit amet nisl nec arcu mollis faucibus tempor id ante. Curabitur commodo vestibulum nulla at lacinia. Cras venenatis, massa et imperdiet ultrices, ante nisl imperdiet magna, nec mollis ante nisl vitae eros. Curabitur consequat facilisis lectus a consequat. Aliquam erat volutpat. Sed est lacus, egestas a risus posuere, pharetra elementum risus. Maecenas ullamcorper tortor ac eros adipiscing, ac commodo purus varius.</p>
+			
+		</div><!-- sec_five_content -->
+		
+		<div class="sec_five_image_wrapper">
+			
+			<div class="sec_five_image_one">
+			
+				<img src="<?php bloginfo('template_directory');?>/images/img-1.jpg"/>
+			
+			</div><!-- sec_five_image -->
+			
+			<div class="sec_five_image_two">
+			
+				<img src="<?php bloginfo('template_directory');?>/images/img-2.jpg"/>
+				
+			</div><!-- sec_five_image -->
+			
+			<div class="sec_five_image_cap">
+				
+				<div class="est_wrapper">
+					
+					<span class="est">Est.</span>
+					
+					<span class="year">1927</span>
+					
+				</div><!-- est_wrapper -->
+				
+				<div class="sec_five_blue_cap">
+				
+				<span>We have some of the Most</span>
+				<span>Well-known + Experienced Trial</span>
+				<span>Attorneys in the State.</span>
+				
+			</div><!-- sec_five_blue_cap -->
+				
+		</div><!-- sec_five_image_cap -->
+			
+	</div><!-- sec_five_image_wrapper -->
+		
+	<div class="sec_five_content bottom content">
+		
+		<h2>Nullam tempus orci lacus sit amet itels scelerisque ligula vestibulum vitae lacus sit amet itels scelerisque ligula vestibulum. Nullam tempus orci lacus sit amet itels scelerisque ligula vestibulum vitae lacus sit amet itels scelerisque ligula vestibulum </h2>
+
+		<p>Nam pharetra, dui semper imperdiet dictum, ipsum augue auctor nibh, vel porta velit diam eget neque. Nullam fermentum mauris eu massa iaculis, et gravida dolor tincidunt. Duis et pellentesque augue, quis porttitor ligula. Nunc sit amet sem in dolor euismod rutrum. Suspendisse potenti. Quisque at consequat urna. Nam lobortis elementum sapien, a blandit tortor placerat ac. Nam porttitor consectetur dui, sed rhoncus nisl porttitor non.</p>
+		
+		<p>Curabitur eu ornare sapien. Praesent non adipiscing nisi. Aenean adipiscing risus eget feugiat pharetra. Proin malesuada sem lectus, et facilisis ante condimentum a. Phasellus nec tellus tempor, adipiscing orci sit amet, dapibus magna. Phasellus ultricies lacus vitae pharetra pharetra. Nulla eleifend, risus non semper lobortis, massa eros molestie nulla, eget adipiscing felis neque ultrices justo. Quisque facilisis magna ut erat laoreet adipiscing. Pellentesque non suscipit nisi, vel ultricies tellus. Etiam vel porttitor turpis. Lorem ipsum</p> 
+		
+	</div><!-- sec_five_content_bottom -->
+	
+</section><!-- section_five -->
--- wp-content/themes/spbmc/page-templates/template-home.php
+++ wp-content/themes/spbmc/page-templates/template-home.php
@@ -8,6 +8,7 @@
 	get_template_part( 'page-templates/homepage_template_parts/section', '1' );
 	get_template_part( 'page-templates/homepage_template_parts/section', '2' );
 	get_template_part( 'page-templates/homepage_template_parts/section', '4' );
+	get_template_part( 'page-templates/homepage_template_parts/section', '5' );
 	
 	
 	get_footer(); ?>
